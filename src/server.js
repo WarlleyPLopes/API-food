3c72a80a6d07de234f7249d104f4b49ade1c5ad5
Warlley Lopes <warlleylopes123@gmail.com>
2024-05-18 17:43:17 +0100

    password change functionality

--- src/server.js
+++ src/server.js
@@ -1,34 +1,34 @@
-require("express-async-errors");
+require("express-async-errors")
 
-const migrationsRun = require("./database/sqlite/migrations");
-const AppError = require("./utils/AppError");
+const migrationsRun = require("./database/sqlite/migrations")
+const AppError = require("./utils/AppError")
 
-const express = require("express");
-const routes = require("./routes");
+const express = require("express")
+const routes = require("./routes")
 
-migrationsRun();
+migrationsRun()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use(routes);
+app.use(routes)
 
 app.use((error, req, res, next) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: "error",
       message: error.message,
-    });
+    })
   }
 
-  console.error(error);
+  console.error(error)
 
   return res.status(500).json({
     status: "error",
     message: "Internal  server error",
-  });
-});
+  })
+})
 
-const PORT = 3333;
+const PORT = 3333
 
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
