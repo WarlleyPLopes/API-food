e74b178a43d884e17ab94f8bd07c3810a7a7151c
Warlley Lopes <warlleylopes123@gmail.com>
2024-09-01 18:16:08 +0100

    fix deploy code

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors")
 
 const AppError = require("./utils/AppError")
@@ -31,6 +32,6 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
